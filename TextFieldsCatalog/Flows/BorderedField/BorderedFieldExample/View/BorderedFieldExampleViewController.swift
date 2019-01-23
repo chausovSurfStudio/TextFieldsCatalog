eebbf84802ca9274ef71a7e10450e972b5537649
Alexander Chausov <chausov@surfstudio.ru>
2019-01-23 17:47:47 +0300

    added example to the first field

--- TextFieldsCatalog/Flows/BorderedField/BorderedFieldExample/View/BorderedFieldExampleViewController.swift
+++ TextFieldsCatalog/Flows/BorderedField/BorderedFieldExample/View/BorderedFieldExampleViewController.swift
@@ -13,6 +13,8 @@ final class BorderedFieldExampleViewController: UIViewController {
     // MARK: - IBOutlets
 
     @IBOutlet private weak var closeButton: UIButton!
+    @IBOutlet private weak var resetButton: UIButton!
+    @IBOutlet private weak var textField: BorderedTextField!
 
     // MARK: - Properties
 
@@ -36,8 +38,24 @@ final class BorderedFieldExampleViewController: UIViewController {
 extension BorderedFieldExampleViewController: BorderedFieldExampleViewInput {
 
     func setupInitialState() {
-        view.backgroundColor = Color.Main.background
-        closeButton.setImage(UIImage(asset: Asset.close), for: .normal)
+        configureAppearance()
+
+        textField.configure(placeholder: "Пароль", maxLength: nil)
+        textField.configure(correction: .no, keyboardType: .asciiCapable)
+        textField.disablePasteAction()
+        textField.setHint("Текст подсказки")
+        textField.setReturnKeyType(.next)
+        textField.setTextFieldMode(.password)
+
+        let validator = TextFieldValidator(minLength: 8, maxLength: 20, regex: Regex.password)
+        validator.shortErrorText = "Пароль слишком короткий"
+        textField.validator = validator
+
+        textField.maskFormatter = MaskTextFieldFormatter(mask: FormatterMasks.password)
+
+        textField.onShouldReturn = { [weak self] _ in
+            self?.textField.resignFirstResponder()
+        }
     }
 
 }
@@ -48,6 +66,24 @@ private extension BorderedFieldExampleViewController {
 
     @IBAction func tapOnCloseButton(_ sender: Any) {
         output?.close()
+    }
+
+    @IBAction func tapOnResetButton(_ sender: Any) {
+        textField.reset()
+    }
+
+}
+
+// MARK: - Configure
+
+private extension BorderedFieldExampleViewController {
+
+    func configureAppearance() {
+        view.backgroundColor = Color.Main.background
+        closeButton.setImage(UIImage(asset: Asset.close), for: .normal)
+        closeButton.tintColor = Color.Main.active
+        resetButton.setTitle("Сбросить", for: .normal)
+        resetButton.tintColor = Color.Main.active
     }
 
 }
