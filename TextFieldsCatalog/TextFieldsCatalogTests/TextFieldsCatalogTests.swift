914feb94cc153d79d6d20311921d540c426b6a27
Alexander Chausov <chausov@surfstudio.ru>
2019-01-28 18:23:47 +0300

    example project moving into the example directory, create project for the pod

---
+++ TextFieldsCatalog/TextFieldsCatalogTests/TextFieldsCatalogTests.swift
@@ -0,0 +1,34 @@
+//
+//  TextFieldsCatalogTests.swift
+//  TextFieldsCatalogTests
+//
+//  Created by Александр Чаусов on 28/01/2019.
+//  Copyright © 2019 Александр Чаусов. All rights reserved.
+//
+
+import XCTest
+@testable import TextFieldsCatalog
+
+class TextFieldsCatalogTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
