cea26120e1c24c70e37147f29a9c9258bfde80c3
Alexander Chausov <chausov@surfstudio.ru>
2019-02-10 18:04:49 +0300

    made several methods for the formatter mask public for separate reuse

--- TextFieldsCatalog/Library/Utils/MaskTextFieldFormatter.swift
+++ TextFieldsCatalog/Library/Utils/MaskTextFieldFormatter.swift
@@ -15,9 +15,9 @@ public final class MaskTextFieldFormatter: NSObject {
     // MARK: - Properties
 
     /// Error message for case, when user leave empty text field
-    var emptyStringErrorMessage: String = L10n.Errors.Textfield.empty
+    public var emptyStringErrorMessage: String = L10n.Errors.Textfield.empty
     /// Error message for case, when user leave incorrect text field
-    var incorrectStringErrorMessage: String = L10n.Errors.Textfield.notValid
+    public var incorrectStringErrorMessage: String = L10n.Errors.Textfield.notValid
 
     // MARK: - Private Properties
 
@@ -41,7 +41,7 @@ public final class MaskTextFieldFormatter: NSObject {
     // MARK: - Internal Methods
 
     /// Method returns tuple with isValid flag and errorMessage, if current input state is incorrect
-    func validate() -> (isValid: Bool, errorMessage: String?) {
+    public func validate() -> (isValid: Bool, errorMessage: String?) {
         if inputIsCompleted {
             return (true, nil)
         }
@@ -52,18 +52,18 @@ public final class MaskTextFieldFormatter: NSObject {
     }
 
     /// Method for formatting passed string with initial mask
-    func format(string: String?, field: UITextField) {
+    public func format(string: String?, field: UITextField) {
         let text = string ?? ""
         maskedDelegate.put(text: text, into: field)
     }
 
     /// Method returns object, which you must access to delegate property of needed UITextField
-    func delegateForTextField() -> UITextFieldDelegate {
+    public func delegateForTextField() -> UITextFieldDelegate {
         return maskedDelegate
     }
 
     /// Method for setting object as listener to provide processing callbacks of UITextFieldDelegate messages
-    func setListenerToFormatter(listener: MaskedTextFieldDelegateListener) {
+    public func setListenerToFormatter(listener: MaskedTextFieldDelegateListener) {
         maskedDelegate.listener = listener
     }
 
