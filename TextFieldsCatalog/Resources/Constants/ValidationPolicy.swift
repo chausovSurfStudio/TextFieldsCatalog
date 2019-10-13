d1530fe4293b8ef580b65886a4fb75d7f5891421
Александр Чаусов <chausov@surfstudio.ru>
2019-10-13 15:41:33 +0300

    added validation policu after text end editing

---
+++ TextFieldsCatalog/Resources/Constants/ValidationPolicy.swift
@@ -0,0 +1,15 @@
+//
+//  ValidationPolicy.swift
+//  TextFieldsCatalog
+//
+//  Created by Александр Чаусов on 13/10/2019.
+//  Copyright © 2019 Александр Чаусов. All rights reserved.
+//
+
+import Foundation
+
+public enum ValidationPolicy {
+    case always
+    case notEmptyText
+    case afterChanges
+}
