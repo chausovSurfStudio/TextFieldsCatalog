369a89192d907eaa27dd9437a8a03c73f05dceb3
Александр Чаусов <chausov@surfstudio.ru>
2019-10-13 15:44:52 +0300

    added some comments

--- TextFieldsCatalog/Resources/Constants/ValidationPolicy.swift
+++ TextFieldsCatalog/Resources/Constants/ValidationPolicy.swift
@@ -8,8 +8,13 @@
 
 import Foundation
 
+/// Allows you to manage validation behaviors after text field end editing
 public enum ValidationPolicy {
+    /// Validation always performs after text field end editing
     case always
+    /// Validation performs if current text is not empty
     case notEmptyText
+    /// Validation performs if user make some changes into the text
+    /// (entered at least one character, set the text, or manually validat the field)
     case afterChanges
 }
