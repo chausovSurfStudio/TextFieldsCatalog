1cce90a616b5475596373520bf1e5418f35520ac
Дмитрий Йорданов <jordanov@surfstudio.ru>
2020-07-28 16:25:12 +0300

    delete override methods for secure text entry

--- TextFieldsCatalog/Library/BaseClasses/InnerTextField.swift
+++ TextFieldsCatalog/Library/BaseClasses/InnerTextField.swift
@@ -54,23 +54,6 @@ public final class InnerTextField: UITextField {
         return bounds.inset(by: placeholderPadding)
     }
 
-    override public var isSecureTextEntry: Bool {
-        didSet {
-            if isFirstResponder {
-                _ = becomeFirstResponder()
-            }
-        }
-    }
-
-    override public func becomeFirstResponder() -> Bool {
-        let success = super.becomeFirstResponder()
-        if isSecureTextEntry, let text = self.text {
-            self.text?.removeAll()
-            insertText(text)
-        }
-        return success
-    }
-
     // MARK: - Internal Methods
 
     public func fixCursorPosition() {
