cc24f225a7ec4fc9c6af1074c6cdb6d7bacbfe8f
Александр Чаусов <chausov@surfstudio.ru>
2020-10-10 16:55:30 +0300

    added default implementation for protocol

--- TextFieldsCatalog/Library/Protocols/ToolBarInterface.swift
+++ TextFieldsCatalog/Library/Protocols/ToolBarInterface.swift
@@ -9,9 +9,23 @@
 /// Abstract protocol for fields input accessory views.
 /// You can add buttons to switch between input fields, guidedField will help you with this.
 /// Or you can ignore this field and create your own custom toolbar!
+/// Because protocol have default implementation and all of it's components is optional.
 public protocol ToolBarInterface: UIView {
     /// Field that allows you to manage responder chain from your views
     var guidedField: GuidedTextField? { get set }
     /// In this method you have to update your toolbars's appearance via field states
     func updateNavigationButtons()
+}
+
+public extension ToolBarInterface {
+    var guidedField: GuidedTextField? {
+        get {
+            return nil
+        }
+        set {
+        }
+    }
+
+    func updateNavigationButtons() {
+    }
 }
