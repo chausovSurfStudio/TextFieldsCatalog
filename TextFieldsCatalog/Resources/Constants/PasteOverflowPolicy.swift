83d33de93c4cd909c9f2038308c2bdfeb6da497f
Olesya Tranina <tranina.o@gmail.com>
2021-04-26 12:44:07 +0300

    fix header

--- TextFieldsCatalog/Resources/Constants/PasteOverflowPolicy.swift
+++ TextFieldsCatalog/Resources/Constants/PasteOverflowPolicy.swift
@@ -2,11 +2,14 @@
 //  PasteOverflowPolicy.swift
 //  TextFieldsCatalog
 //
+//  Created by Olesya Tranina on 26.04.2021.
+//  Copyright © 2021 Александр Чаусов. All rights reserved.
+//
 
 /// Allows you to manage paste behaviors for text fields that use maxLength
 public enum PasteOverflowPolicy {
     /// Pastes nothing if the text cannot fit completely
     case noChanges
-    /// Pastes part of text that can fit 
+    /// Pastes part of text that can fit
     case textThatFits
 }
